9e55c2c58d84b06886ab951a96989b899b3d0c47
Rakshanda Hedaoo <rakshandaheda254@gmail.com>
2022-10-17 13:38:18 +0530

    3rd commit done

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
   public static void main(String[] args){
     int num1 = 5;
     int num2 = 4;
-    System.out.println(num1+num2);
+    System.out.println("The sum of the numbers is " + (num1+num2);
   }
 }
