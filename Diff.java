ebd92f5e200d2bf97cfd993568c75a491e294ba5
Rakshanda Hedaoo <rakshandaheda254@gmail.com>
2022-10-17 13:37:02 +0530

    2nd commit done

---
+++ Diff.java
@@ -0,0 +1,11 @@
+public class Diff{
+  public static void main(String[] args){
+    int num1 = 5;
+    int num2 = 4;
+    if(num1>num2){
+      System.out.println("The difference of the numbers is " + (num1-num2);
+    }else{
+      System.out.println("The difference of the numbers is " + (num2-num1);
+    }
+  }
+}
