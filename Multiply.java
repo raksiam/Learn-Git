c13563a4c498567a484c5765440bf25b6b021e31
Rakshanda Hedaoo <rakshandaheda254@gmail.com>
2022-10-17 15:25:25 +0530

    4th commit done

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+  public static void main(String[] args){
+    int num1 = 5;
+    int num2 = 4;
+    System.out.println("The product of the numbers is " + (num1*num2);
+  }
+}
